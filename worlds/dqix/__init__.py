b90e7bf22bc5f356675df63e24d77319f14fcde4
Tobias Franz <cinex.mail@web.de>
2026-04-16 14:23:30 +0200

    * Finished transition to RuleBuilder * Moved completion condition into a separate function * Region diagram will now only be generated when in debug mode
    Signed-off-by: Tobias Franz <tobias.franz@mydata.de>

--- worlds/dqix/__init__.py
+++ worlds/dqix/__init__.py
@@ -1,9 +1,10 @@
 from BaseClasses import Tutorial, ItemClassification, Region
 from Utils import visualize_regions
-from rule_builder.rules import Has, HasAll
+from rule_builder.rules import Has, HasAll, Rule
 from .Client import DQIXClient
 from .Items import DQIXItems, ItemType, DQIXItem
 from .Locations import DQIXLocations, DQIXLocation
+from .helper.BaseHelper import BaseHelper
 from ..AutoWorld import World, WebWorld
 
 
@@ -62,12 +63,13 @@ class DragonQuestIX(World):
             self.multiworld.regions.append(region)
 
         # Goal condition
-        self.set_completion_rule(Has("Defeated Corvus (II)"))
+        self.set_completion_rule(self.get_completion_condition())
 
-        state = self.multiworld.get_all_state(use_cache=False, allow_partial_entrances=True)
-        state.update_reachable_regions(self.player)
-        visualize_regions(root_region=self.multiworld.get_region(self.origin_region_name, self.player), file_name="my_world.puml", show_entrance_names=True,
-                          regions_to_highlight=state.reachable_regions[self.player])
+        if BaseHelper.DEBUG_MODE:
+            state = self.multiworld.get_all_state(use_cache=False, allow_partial_entrances=True)
+            state.update_reachable_regions(self.player)
+            visualize_regions(root_region=self.multiworld.get_region(self.origin_region_name, self.player), file_name="my_world.puml", show_entrance_names=True,
+                              regions_to_highlight=set(state.reachable_regions[self.player]))
 
     def create_and_connect_regions(self):
         region_angel_falls = Region("Angel Falls", self.player, self.multiworld)
@@ -77,28 +79,26 @@ class DragonQuestIX(World):
         region_hexagon = Region("Hexagon", self.player, self.multiworld)
         region_hexagon.add_locations(locations=self.location_helper.get_locations_for_group(region_hexagon.name), location_type=DQIXLocation)
 
-        region_hexagon.add_event(location_name="Boss: Hexagon", item_name="Defeated Hexagoon", rule=lambda current_state: current_state.has(item="Boss Key: Hexagoon", player=self.player, count=1))
+        region_hexagon.add_event(location_name="Boss: Hexagon", item_name="Defeated Hexagoon", rule=Has("Boss Key: Hexagoon"))
 
         region_stornway = Region("Stornway", self.player, self.multiworld)
         region_stornway.add_locations(locations=self.location_helper.get_locations_for_group(region_stornway.name), location_type=DQIXLocation)
 
-        region_stornway.add_event(location_name="Boss: Wight Knight", item_name="Defeated Wight Knight",
-                                  rule=lambda current_state: current_state.has(item="Boss Key: Wight Knight", player=self.player, count=1))
+        region_stornway.add_event(location_name="Boss: Wight Knight", item_name="Defeated Wight Knight", rule=Has("Boss Key: Wight Knight"))
 
         region_zere = Region("Zere", self.player, self.multiworld)
         region_zere.add_locations(locations=self.location_helper.get_locations_for_group(region_zere.name), location_type=DQIXLocation)
         region_brigadoom = Region("Brigadoom", self.player, self.multiworld)
         region_brigadoom.add_locations(locations=self.location_helper.get_locations_for_group(region_brigadoom.name), location_type=DQIXLocation)
 
-        region_brigadoom.add_event(location_name="Boss: Morag", item_name="Defeated Morag", rule=lambda current_state: current_state.has(item="Boss Key: Morag", player=self.player, count=1))
+        region_brigadoom.add_event(location_name="Boss: Morag", item_name="Defeated Morag", rule=Has("Boss Key: Morag"))
 
         region_coffinwell = Region("Coffinwell", self.player, self.multiworld)
         region_coffinwell.add_locations(locations=self.location_helper.get_locations_for_group(region_coffinwell.name), location_type=DQIXLocation)
         region_quarantomb = Region("Quarantomb", self.player, self.multiworld)
         region_quarantomb.add_locations(locations=self.location_helper.get_locations_for_group(region_quarantomb.name), location_type=DQIXLocation)
 
-        region_quarantomb.add_event(location_name="Boss: Ragin' Contagion", item_name="Defeated Ragin' Contagion",
-                                    rule=lambda current_state: current_state.has(item="Boss Key: Ragin' Contagion", player=self.player, count=1))
+        region_quarantomb.add_event(location_name="Boss: Ragin' Contagion", item_name="Defeated Ragin' Contagion", rule=Has("Boss Key: Ragin' Contagion"))
 
         region_observatory = Region("Observatory", self.player, self.multiworld)
         region_observatory.add_locations(locations=self.location_helper.get_locations_for_group(region_observatory.name), location_type=DQIXLocation)
@@ -108,16 +108,14 @@ class DragonQuestIX(World):
         region_tower_of_trades = Region("Tower of Trades", self.player, self.multiworld)
         region_tower_of_trades.add_locations(locations=self.location_helper.get_locations_for_group(region_tower_of_trades.name), location_type=DQIXLocation)
 
-        region_tower_of_trades.add_event(location_name="Boss: Master of Nu'un", item_name="Defeated Master of Nu'un",
-                                         rule=lambda current_state: current_state.has(item="Boss Key: Master of Nu'un", player=self.player, count=1))
+        region_tower_of_trades.add_event(location_name="Boss: Master of Nu'un", item_name="Defeated Master of Nu'un", rule=Has("Boss Key: Master of Nu'un"))
 
         region_porth_llaffan = Region("Porth Llaffan", self.player, self.multiworld)
         region_porth_llaffan.add_locations(locations=self.location_helper.get_locations_for_group(region_porth_llaffan.name), location_type=DQIXLocation)
         region_tywll_cave = Region("Tywll Cave", self.player, self.multiworld)
         region_tywll_cave.add_locations(locations=self.location_helper.get_locations_for_group(region_tywll_cave.name), location_type=DQIXLocation)
 
-        region_tywll_cave.add_event(location_name="Boss: Lleviathan", item_name="Defeated Lleviathan",
-                                    rule=lambda current_state: current_state.has(item="Boss Key: Lleviathan", player=self.player, count=1))
+        region_tywll_cave.add_event(location_name="Boss: Lleviathan", item_name="Defeated Lleviathan", rule=Has("Boss Key: Lleviathan"))
 
         region_slurry_quay = Region("Slurry Quay", self.player, self.multiworld)
         region_slurry_quay.add_locations(locations=self.location_helper.get_locations_for_group(region_slurry_quay.name), location_type=DQIXLocation)
@@ -133,16 +131,14 @@ class DragonQuestIX(World):
         region_zere_rocks = Region("Zere Rocks", self.player, self.multiworld)
         region_zere_rocks.add_locations(locations=self.location_helper.get_locations_for_group(region_zere_rocks.name), location_type=DQIXLocation)
 
-        region_zere_rocks.add_event(location_name="Boss: Garth Goyle", item_name="Defeated Garth Goyle",
-                                    rule=lambda current_state: current_state.has(item="Boss Key: Garth Goyle", player=self.player, count=1))
+        region_zere_rocks.add_event(location_name="Boss: Garth Goyle", item_name="Defeated Garth Goyle", rule=Has("Boss Key: Garth Goyle"))
 
         region_bloomingdale = Region("Bloomingdale", self.player, self.multiworld)
         region_bloomingdale.add_locations(locations=self.location_helper.get_locations_for_group(region_bloomingdale.name), location_type=DQIXLocation)
         region_bad_cave = Region("Bad Cave", self.player, self.multiworld)
         region_bad_cave.add_locations(locations=self.location_helper.get_locations_for_group(region_bad_cave.name), location_type=DQIXLocation)
 
-        region_bad_cave.add_event(location_name="Boss: Tyrantula", item_name="Defeated Tyrantula",
-                                  rule=lambda current_state: current_state.has(item="Boss Key: Tyrantula", player=self.player, count=1))
+        region_bad_cave.add_event(location_name="Boss: Tyrantula", item_name="Defeated Tyrantula", rule=Has("Boss Key: Tyrantula"))
 
         region_ocean = Region("Ocean", self.player, self.multiworld)
         region_ocean.add_locations(locations=self.location_helper.get_locations_for_group(region_ocean.name), location_type=DQIXLocation)
@@ -154,55 +150,47 @@ class DragonQuestIX(World):
         region_plumbed_depths = Region("Plumbed Depths", self.player, self.multiworld)
         region_plumbed_depths.add_locations(locations=self.location_helper.get_locations_for_group(region_plumbed_depths.name), location_type=DQIXLocation)
 
-        region_plumbed_depths.add_event(location_name="Boss: Grand Lizzier", item_name="Defeated Grand Lizzier",
-                                        rule=lambda current_state: current_state.has(item="Boss Key: Grand Lizzier", player=self.player, count=1))
+        region_plumbed_depths.add_event(location_name="Boss: Grand Lizzier", item_name="Defeated Grand Lizzier", rule=Has("Boss Key: Grand Lizzier"))
 
         region_batsureg = Region("Batsureg", self.player, self.multiworld)
         region_batsureg.add_locations(locations=self.location_helper.get_locations_for_group(region_batsureg.name), location_type=DQIXLocation)
         region_gerzuun = Region("Gerzuun", self.player, self.multiworld)
         region_gerzuun.add_locations(locations=self.location_helper.get_locations_for_group(region_gerzuun.name), location_type=DQIXLocation)
 
-        region_gerzuun.add_event(location_name="Boss: Larstastnaras", item_name="Defeated Larstastnaras",
-                                 rule=lambda current_state: current_state.has(item="Boss Key: Larstastnaras", player=self.player, count=1))
+        region_gerzuun.add_event(location_name="Boss: Larstastnaras", item_name="Defeated Larstastnaras", rule=Has("Boss Key: Larstastnaras"))
 
         region_swinedimpels = Region("Swinedimpels Academy", self.player, self.multiworld)
         region_swinedimpels.add_locations(locations=self.location_helper.get_locations_for_group(region_swinedimpels.name), location_type=DQIXLocation)
         region_old_school = Region("Old School", self.player, self.multiworld)
         region_old_school.add_locations(locations=self.location_helper.get_locations_for_group(region_old_school.name), location_type=DQIXLocation)
 
-        region_old_school.add_event(location_name="Boss: Dreadmaster", item_name="Defeated Dreadmaster",
-                                    rule=lambda current_state: current_state.has(item="Boss Key: Dreadmaster", player=self.player, count=1))
+        region_old_school.add_event(location_name="Boss: Dreadmaster", item_name="Defeated Dreadmaster", rule=Has("Boss Key: Dreadmaster"))
 
         region_wormwood_creek = Region("Wormwood Creek", self.player, self.multiworld)
         region_wormwood_creek.add_locations(locations=self.location_helper.get_locations_for_group(region_wormwood_creek.name), location_type=DQIXLocation)
         region_bowhole = Region("Bowhole", self.player, self.multiworld)
         region_bowhole.add_locations(locations=self.location_helper.get_locations_for_group(region_bowhole.name), location_type=DQIXLocation)
 
-        region_bowhole.add_event(location_name="Boss: Gadrongo", item_name="Defeated Gadrongo", rule=lambda current_state: current_state.has(item="Boss Key: Gadrongo", player=self.player, count=1))
+        region_bowhole.add_event(location_name="Boss: Gadrongo", item_name="Defeated Gadrongo", rule=Has("Boss Key: Gadrongo"))
 
         region_upover = Region("Upover", self.player, self.multiworld)
         region_upover.add_locations(locations=self.location_helper.get_locations_for_group(region_upover.name), location_type=DQIXLocation)
         region_magmaroo = Region("Magmaroo", self.player, self.multiworld)
         region_magmaroo.add_locations(locations=self.location_helper.get_locations_for_group(region_magmaroo.name), location_type=DQIXLocation)
 
-        region_magmaroo.add_event(location_name="Boss: Greygnarl", item_name="Defeated Greygnarl",
-                                  rule=lambda current_state: current_state.has(item="Boss Key: Greygnarl", player=self.player, count=1))
+        region_magmaroo.add_event(location_name="Boss: Greygnarl", item_name="Defeated Greygnarl", rule=Has("Boss Key: Greygnarl"))
 
         region_goretress = Region("Goretress", self.player, self.multiworld)
         region_goretress.add_locations(locations=self.location_helper.get_locations_for_group(region_goretress.name), location_type=DQIXLocation)
 
-        region_goretress.add_event(location_name="Boss: Goreham-Hogg (I)", item_name="Defeated Goreham-Hogg (I)",
-                                   rule=lambda current_state: current_state.has(item="Boss Key: Goreham-Hogg (I)", player=self.player, count=1))
+        region_goretress.add_event(location_name="Boss: Goreham-Hogg (I)", item_name="Defeated Goreham-Hogg (I)", rule=Has("Boss Key: Goreham-Hogg (I)"))
 
         region_gittingham_palace = Region("Gittingham Palace", self.player, self.multiworld)
         region_gittingham_palace.add_locations(locations=self.location_helper.get_locations_for_group(region_gittingham_palace.name), location_type=DQIXLocation)
 
-        region_gittingham_palace.add_event(location_name="Boss: Hootingham-Gore (I)", item_name="Defeated Hootingham-Gore (I)",
-                                           rule=lambda current_state: current_state.has(item="Boss Key: Hootingham-Gore (I)", player=self.player, count=1))
-        region_gittingham_palace.add_event(location_name="Boss: Goresby-Purrvis (I)", item_name="Defeated Goresby-Purrvis (I)",
-                                           rule=lambda current_state: current_state.has(item="Boss Key: Goresby-Purrvis (I)", player=self.player, count=1))
-        region_gittingham_palace.add_event(location_name="Boss: King Godwyn", item_name="Defeated King Godwyn",
-                                           rule=lambda current_state: current_state.has(item="Boss Key: King Godwyn", player=self.player, count=1))
+        region_gittingham_palace.add_event(location_name="Boss: Hootingham-Gore (I)", item_name="Defeated Hootingham-Gore (I)", rule=Has("Boss Key: Hootingham-Gore (I)"))
+        region_gittingham_palace.add_event(location_name="Boss: Goresby-Purrvis (I)", item_name="Defeated Goresby-Purrvis (I)", rule=Has("Boss Key: Goresby-Purrvis (I)"))
+        region_gittingham_palace.add_event(location_name="Boss: King Godwyn", item_name="Defeated King Godwyn", rule=Has("Boss Key: King Godwyn"))
 
         region_oubliette = Region("Oubliette", self.player, self.multiworld)
         region_oubliette.add_locations(locations=self.location_helper.get_locations_for_group(region_oubliette.name), location_type=DQIXLocation)
@@ -210,19 +198,13 @@ class DragonQuestIX(World):
         region_realm_of_the_mighty = Region("Realm of the Mighty", self.player, self.multiworld)
         region_realm_of_the_mighty.add_locations(locations=self.location_helper.get_locations_for_group(region_realm_of_the_mighty.name), location_type=DQIXLocation)
 
-        region_realm_of_the_mighty.add_event(location_name="Boss: Goreham-Hogg (II)", item_name="Defeated Goreham-Hogg (II)",
-                                             rule=lambda current_state: current_state.has(item="Boss Key: Goreham-Hogg (II)", player=self.player, count=1))
-        region_realm_of_the_mighty.add_event(location_name="Boss: Hootingham-Gore (II)", item_name="Defeated Hootingham-Gore (II)",
-                                             rule=lambda current_state: current_state.has(item="Boss Key: Hootingham-Gore (II)", player=self.player, count=1))
-        region_realm_of_the_mighty.add_event(location_name="Boss: Goresby-Purrvis (II)", item_name="Defeated Goresby-Purrvis (II)",
-                                             rule=lambda current_state: current_state.has(item="Boss Key: Goresby-Purrvis (II)", player=self.player, count=1))
+        region_realm_of_the_mighty.add_event(location_name="Boss: Goreham-Hogg (II)", item_name="Defeated Goreham-Hogg (II)", rule=Has("Boss Key: Goreham-Hogg (II)"))
+        region_realm_of_the_mighty.add_event(location_name="Boss: Hootingham-Gore (II)", item_name="Defeated Hootingham-Gore (II)", rule=Has("Boss Key: Hootingham-Gore (II)"))
+        region_realm_of_the_mighty.add_event(location_name="Boss: Goresby-Purrvis (II)", item_name="Defeated Goresby-Purrvis (II)", rule=Has("Boss Key: Goresby-Purrvis (II)"))
 
-        region_realm_of_the_mighty.add_event(location_name="Boss: Corvus (I)", item_name="Defeated Corvus (I)",
-                                             rule=lambda current_state: current_state.has(item="Boss Key: Corvus (I)", player=self.player, count=1))
-        region_realm_of_the_mighty.add_event(location_name="Boss: Barbarus", item_name="Defeated Barbarus",
-                                             rule=lambda current_state: current_state.has(item="Boss Key: Barbarus", player=self.player, count=1))
-        region_realm_of_the_mighty.add_event(location_name="Boss: Corvus (II)", item_name="Defeated Corvus (II)",
-                                             rule=lambda current_state: current_state.has(item="Boss Key: Corvus (II)", player=self.player, count=1))
+        region_realm_of_the_mighty.add_event(location_name="Boss: Corvus (I)", item_name="Defeated Corvus (I)", rule=Has("Boss Key: Corvus (I)"))
+        region_realm_of_the_mighty.add_event(location_name="Boss: Barbarus", item_name="Defeated Barbarus", rule=Has("Boss Key: Barbarus"))
+        region_realm_of_the_mighty.add_event(location_name="Boss: Corvus (II)", item_name="Defeated Corvus (II)", rule=Has("Boss Key: Corvus (II)"))
 
         # Connecting all the regions
 
@@ -254,7 +236,7 @@ class DragonQuestIX(World):
 
         self.create_entrance(region_heights_of_loneliness, region_zere_rocks)
 
-        self.create_entrance(region_bloomingdale, region_ocean, Has("Defeated Tyrantula"))  # Check if player has unlocked ship
+        self.create_entrance(region_bloomingdale, region_ocean, Has("Defeated Tyrantula"))
 
         self.create_entrance(region_ocean, region_gleeba)
         self.create_entrance(region_ocean, region_batsureg)
@@ -288,3 +270,6 @@ class DragonQuestIX(World):
 
     def get_filler_item_name(self) -> str:
         return self.random.choice(self.item_helper.get_filler_item_names())
+
+    def get_completion_condition(self) -> Rule:
+        return Has("Defeated Corvus (II)")
